5b92351d8bb57e0fa5ebe5a1aed90cc3627bfd55
remco van der Velde <remco.pc@outlook.com>
2023-12-06 01:12:35 +0100

    debugging info all command

---
+++ src/Output/Filter/System/Config.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace R3m\Io\Output\Filter\System;
+
+use R3m\Io\App;
+
+use R3m\Io\Module\Controller;
+
+class Config extends Controller {
+    const DIR = __DIR__ . '/';
+
+    public static function log(App $object, $response=null): object
+    {
+        $result = [];
+        if(
+            !empty($response) &&
+            is_array($response)
+        ){
+            foreach($response as $nr => $record){
+                if(
+                    is_array($record) &&
+                    array_key_exists('name', $record)
+                ){
+                    $result[$record['name']] = $record;
+                }
+                elseif(
+                    is_object($record) &&
+                    property_exists($record, 'name')
+                ){
+                    $result[$record->name] = $record;
+                }
+            }
+        }
+        return (object) $result;
+    }
+}
---
+++ src/Output/Filter/System/Server.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace R3m\Io\Output\Filter\System;
+
+use R3m\Io\App;
+
+use R3m\Io\Module\Controller;
+
+class Server extends Controller {
+    const DIR = __DIR__ . '/';
+
+    public static function url(App $object, $response=null): object
+    {
+        $result = [];
+        if(
+            !empty($response) &&
+            is_array($response)
+        ){
+            foreach($response as $nr => $record){
+                if(
+                    is_array($record) &&
+                    array_key_exists('name', $record) &&
+                    array_key_exists('options', $record)
+                ){
+                    $result[$record['name']] = $record['options'];
+                }
+                elseif(
+                    is_object($record) &&
+                    property_exists($record, 'name') &&
+                    property_exists($record, 'options')
+                ){
+                    $result[$record->name] = $record->options;
+                }
+            }
+        }
+        return (object) $result;
+    }
+}
