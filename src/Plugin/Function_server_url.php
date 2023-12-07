54833ed684b90b8f614d1983e912b9be59c0e765
remco van der Velde <remco.pc@outlook.com>
2023-12-07 18:19:34 +0100

    configure through node

--- src/Plugin/Function_server_url.php
+++ src/Plugin/Function_server_url.php
@@ -6,6 +6,7 @@ use R3m\Io\Config;
 
 function function_server_url(Parse $parse, Data $data, $name=''){
     $object = $parse->object();
+    echo 'fuck';
     d($object->config('server.url'));
     ddd($object->config('server.url.' . $name . '.' . $object->config('framework.environment')));
 }
