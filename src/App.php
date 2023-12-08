d2e68dbf0701ec5cbf422e464d9fc72eae6288ec
remco van der Velde <remco.pc@outlook.com>
2023-12-08 22:50:13 +0100

    Add SELECT_WILDCARD constant to Route module
    The SELECT_WILDCARD constant has been added in the Route module to handle wildcard routes. Scripts in the module were also updated to use this constant when selecting request routes. The wildcard selector is now applied in the routing process, allowing a broader match for incoming requests.

--- src/App.php
+++ src/App.php
@@ -174,8 +174,6 @@ class App extends Data {
             if ($file === false) {
                 $route = Route::request($object);
                 if ($route === false) {
-                    $route = Route::wildcard($object);
-                    ddd($route);
                     if ($object->config('framework.environment') === Config::MODE_DEVELOPMENT) {
                         if($logger){
                             $object->logger($logger)->error('Couldn\'t determine route (' . $object->request('request') . ')...');
@@ -195,7 +193,6 @@ class App extends Data {
                         return Response::output($object, $response);
                     } else {
                         $route = Route::wildcard($object);
-                        ddd($route);
                         if ($route === false) {
                             if($logger){
                                 $object->logger($logger)->error('Couldn\'t determine route (wildcard) (' . $object->request('request') . ')...');
