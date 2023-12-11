0bf3d470a62c4e8eac776bbab835030ebf345d61
remco van der Velde <remco.pc@outlook.com>
2023-12-11 19:56:59 +0100

    Add debug function in Route.php
    Inserted a debug function (d($host);) in Route.php to log host names. This assists in efficiently tracking routing processes and understanding how host names are being handled in the application.

--- src/Module/Route.php
+++ src/Module/Route.php
@@ -991,6 +991,7 @@ class Route extends Data {
             $object->data(App::ROUTE, $route);
         }
         $host = strtolower($object->config('host.name'));
+        d($host);
         if(empty($host)){
             Route::framework($object);
             $node = new Node($object);
