432f36465586fc24883b0b63bc039b22a22a255c
remco van der Velde <remco.pc@outlook.com>
2023-12-12 23:28:05 +0100

    Add logger null check in App.php
    In App.php, a condition to check if the logger exists was implemented before executing logger related functions. This prevents potential issues involving null logger instances. This update provides robustness to the application, ensuring that logger related functions are only called when a logger is present.

--- src/App.php
+++ src/App.php
@@ -170,6 +170,7 @@ class App extends Data {
             $file = FileRequest::get($object);
             if ($file === false) {
                 $route = Route::request($object);
+                ddd($route);
                 if ($route === false) {
                     if ($object->config('framework.environment') === Config::MODE_DEVELOPMENT) {
                         if($logger){
