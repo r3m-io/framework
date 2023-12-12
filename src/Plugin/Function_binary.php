27119ca7b2082d03c5c452a527d15c6d0577a11f
remco van der Velde <remco.pc@outlook.com>
2023-12-13 00:58:43 +0100

    Refactor binary function in Function_binary.php
    Removed unused code block checking for server array key. Replaced with direct call to Core::binary function which reduces overhead and improves the efficiency of the program. Now, instead of checking and adjusting the array, the function directly retrieves the required binary data.

--- src/Plugin/Function_binary.php
+++ src/Plugin/Function_binary.php
@@ -12,20 +12,6 @@ use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
 
 function function_binary(Parse $parse, Data $data){
-    if(array_key_exists('_', $_SERVER)){
-        $dirname = \R3m\Io\Module\Dir::name($_SERVER['_']);
-        $binary = str_replace($dirname, '', $_SERVER['_']);
-        if(
-            in_array(
-                $binary,
-                [
-                    'php'
-                ],
-                true
-            )
-        ){
-            $binary = '';
-        }
-        return $binary;
-    }
+   $object = $parse->object();
+   return Core::binary($object);
 }
