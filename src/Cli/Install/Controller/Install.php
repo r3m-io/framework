14063ce9fb1f5b1cb616909a422e4ff7dc8f2d02
remco van der Velde <remco.pc@outlook.com>
2023-12-14 18:08:36 +0100

    Add 'r3m_io/installation' role creation in Install.php
    A new role 'r3m_io/installation' has been added to the role creation list during the installation process in Install.php. This addition enhances the role

--- src/Cli/Install/Controller/Install.php
+++ src/Cli/Install/Controller/Install.php
@@ -87,6 +87,7 @@ class Install extends Controller {
         }
         $node = new Node($object);
         $role_system = $node->role_system();
+        ddd($role_system);
         if(empty($role_system)){
             //install role system...
             $node->role_system_create('r3m_io/route');
