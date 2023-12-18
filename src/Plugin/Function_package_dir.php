238006490029fee78d9fbaa01c70c55e938ae874
remco van der Velde <remco.pc@outlook.com>
2023-12-18 20:21:28 +0100

    package dir

--- src/Plugin/Function_package_dir.php
+++ src/Plugin/Function_package_dir.php
@@ -13,11 +13,35 @@ use R3m\Io\Module\Data;
 use R3m\Io\Module\Dir;
 
 function function_package_dir(Parse $parse, Data $data, $prefix='', $package=''){
+    $object = $parse->object();
+    if(empty($prefix)){
+        throw new Exception('Prefix is empty');
+    }
     $explode = explode('_', $package);
     foreach($explode as $nr => $value){
         $explode[$nr] = ucfirst($value);
     }
     $package = implode('/', $explode);
     $package = Dir::ucfirst($package);
-    ddd($package);
+
+    $explode = explode('/', $package);
+    if(substr($prefix, 0, -1) !== '/'){
+        $prefix .= '/';
+    }
+    $result = $prefix . $package;
+    if(Dir::is($result)){
+        return $result;
+    }
+    $dir = $prefix;
+    foreach($explode as $nr => $value){
+        $dir .= $value . '/';
+        Dir::create($dir, Dir::CHMOD);
+        $command = 'chown www-data:www-data ' . $dir;
+        exec($command);
+        if($object->config('framework.environment') === Config::MODE_DEVELOPMENT){
+            $command = 'chmod 777 ' . $dir;
+            exec($command);
+        }
+    }
+    return $result;
 }
