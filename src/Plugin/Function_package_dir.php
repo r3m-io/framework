433f39c1400de0c8e5213561be6ca211bbd3478d
remco van der Velde <remco.pc@outlook.com>
2023-12-18 20:15:06 +0100

    options pass through

--- src/Plugin/Function_package_dir.php
+++ src/Plugin/Function_package_dir.php
@@ -12,11 +12,12 @@ use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
 use R3m\Io\Module\Dir;
 
-function function_package_dir(Parse $parse, Data $data, $package=''){
+function function_package_dir(Parse $parse, Data $data, $prefix='', $package=''){
     $explode = explode('_', $package);
     foreach($explode as $nr => $value){
         $explode[$nr] = ucfirst($value);
     }
     $package = implode('/', $explode);
-    return Dir::ucfirst($package);
+    $package = Dir::ucfirst($package);
+    ddd($package);
 }
