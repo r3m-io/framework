ad193c692986c90d2330488e5df655b7a70c1be4
remco van der Velde <remco.pc@outlook.com>
2023-12-21 19:52:55 +0100

    file::permission

--- src/Config.php
+++ src/Config.php
@@ -453,6 +453,7 @@ class Config extends Data {
         if(!$node->role_has_permission($role_system, 'System:Config:record')){
             return;
         }
+        ddd('yes working');
         $response = $node->record($class, $role_system, $options);
         if(
             $response &&
