09788b9c09329252e69e9c721be685baafacc113
remco van der Velde <remco.pc@outlook.com>
2023-12-23 22:41:00 +0100

    event & output filter

--- src/Module/Middleware.php
+++ src/Module/Middleware.php
@@ -27,6 +27,8 @@ use R3m\Io\Exception\FileWriteException;
 
 class Middleware extends Main {
 
+    const DIR = __DIR__ . DIRECTORY_SEPARATOR;
+
     const NAME = 'Middleware';
     const OBJECT = 'System.Middleware';
 
