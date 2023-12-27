b0bb692d6ca85ff966594efd22562a2ad52f6f69
remco van der Velde <remco.pc@outlook.com>
2023-12-27 11:15:20 +0100

    dir copy was wrong

--- src/Validator/String.Not.Contains.php
+++ src/Validator/String.Not.Contains.php
@@ -15,6 +15,8 @@ function validate_string_not_contains(App $object, $string='', $field='', $argum
     if(empty($string)){
         return false;
     }
+    d($string);
+    d($argument);
     if(is_string($argument)){
         if(stristr($string, $argument) !== false){
             return false;
