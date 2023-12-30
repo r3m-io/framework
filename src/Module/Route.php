408f3517fd458a0f662741b94e280bda218a3a39
remco van der Velde <remco.pc@outlook.com>
2023-12-30 22:24:01 +0100

    debugging route, .local domain is not yet working

--- src/Module/Route.php
+++ src/Module/Route.php
@@ -632,7 +632,6 @@ class Route extends Data {
         }
         $current = false;
         foreach($data as $name => $record){
-            d($record);
             if(property_exists($record, 'resource')){
                 continue;
             }
