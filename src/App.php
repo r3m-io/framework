da2b29b791883a4fa978c930dc581a9478c54702
remco van der Velde <remco.pc@outlook.com>
2024-01-04 20:11:24 +0100

    rsearching for role.system in framework

--- src/App.php
+++ src/App.php
@@ -351,7 +351,7 @@ class App extends Data {
                         $delete_role_uuid =  $object->data('framework.role.system.uuid');
                         $object->data('delete', $delete_role_uuid);
                         d($delete_role_uuid);
-                        ddd($object->data());
+                        ddd($object->data('framework'));
                         //remove role.system from $object
                         $result = $controller::{$function}($object);
                         Event::trigger($object, 'app.run.route.controller', [
