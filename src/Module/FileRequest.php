45050cd37c7e80b7657949f1ecb67e0b2866b0df
remco van der Velde <remco.pc@outlook.com>
2024-01-07 04:12:35 +0100

    return types

--- src/Module/FileRequest.php
+++ src/Module/FileRequest.php
@@ -489,8 +489,6 @@ class FileRequest {
             }
             throw new LocateException('Cannot find location for file...', $location);
         } else {
-            d($file_extension_lowercase);
-            ddd($object->config('error'));
             if(
                 in_array(
                     $file_extension_lowercase,
