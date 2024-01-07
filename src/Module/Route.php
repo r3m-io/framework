c6efa656836de66cec192c52de94f4940723f8a7
remco van der Velde <remco.pc@outlook.com>
2024-01-07 02:08:38 +0100

    return types

--- src/Module/Route.php
+++ src/Module/Route.php
@@ -824,7 +824,7 @@ class Route extends Data {
     public static function controller(object $route): object
     {
         if(property_exists($route, 'controller')){
-            ddd($route);
+            d($route);
             $is_double_colon = str_contains($route->controller, ':');
             if($is_double_colon){
                 $controller = explode(':', $route->controller);
