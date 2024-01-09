5b7c993e187945d6377d1d4b25e6a417ce6410f7
remco van der Velde <remco.pc@outlook.com>
2024-01-09 19:08:07 +0100

    removed project.log.name from the project

--- src/App.php
+++ src/App.php
@@ -178,7 +178,7 @@ class App extends Data {
             $file = FileRequest::get($object);
             if ($file === false) {
                 $destination = Route::request($object);
-                ddd($destination);
+                d($destination);
                 if ($destination === false) {
                     if ($object->config('framework.environment') === Config::MODE_DEVELOPMENT) {
                         if($logger){
