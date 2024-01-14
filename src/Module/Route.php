b9fdfe0a4a72932d3c1357b9910a6611034ce6ac
remco van der Velde <remco.pc@outlook.com>
2024-01-14 21:29:39 +0100

    (phpstan)

--- src/Module/Route.php
+++ src/Module/Route.php
@@ -1109,6 +1109,7 @@ class Route extends Data {
             $object->data(App::ROUTE, $route);
         }
         $host = strtolower($object->config('host.name'));
+        ddd($host);
         if(empty($host) && Core::is_cli()){
             Route::framework($object);
             $node = new Node($object);
