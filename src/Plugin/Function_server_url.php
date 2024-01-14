f22480f151974c67a1d9b80072efdc5fb230d6db
remco van der Velde <remco.pc@outlook.com>
2024-01-14 22:07:57 +0100

    (phpstan)

--- src/Plugin/Function_server_url.php
+++ src/Plugin/Function_server_url.php
@@ -10,5 +10,9 @@ use R3m\Io\Config;
 function function_server_url(Parse $parse, Data $data, $name=''){
     $object = $parse->object();
     $name = str_replace('.', '-', $name);
-    return $object->config('server.url.' . $name . '.' . $object->config('framework.environment'));
+    $url = $object->config('server.url.' . $name . '.' . $object->config('framework.environment'));
+    if($url && substr($url, 0, -1) !== '/'){
+        $url .= '/';
+    }
+    return $url;
 }
