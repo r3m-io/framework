eecd64110e3d997fc96232e56582d54416d289ba
remco van der Velde <remco.pc@outlook.com>
2024-01-14 23:22:09 +0100

    (phpstan)

--- src/App.php
+++ src/App.php
@@ -168,6 +168,7 @@ class App extends Data {
         Handler::request_configure($object);
         App::configure($object);
         Route::configure($object);
+        d($object->data(App::ROUTE));
         $destination = false;
         $logger = false;
         if($object->config('framework.environment') === Config::MODE_DEVELOPMENT){
