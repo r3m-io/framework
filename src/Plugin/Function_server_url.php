a2e2f44f3306d20bd3a4ee955ae22eddc181cfb2
remco van der Velde <remco.pc@outlook.com>
2024-01-14 22:06:10 +0100

    (phpstan)

--- src/Plugin/Function_server_url.php
+++ src/Plugin/Function_server_url.php
@@ -4,9 +4,11 @@ use R3m\Io\Module\Parse;
 use R3m\Io\Module\Data;
 use R3m\Io\Config;
 
+/**
+ * @throws Exception
+ */
 function function_server_url(Parse $parse, Data $data, $name=''){
     $object = $parse->object();
     $name = str_replace('.', '-', $name);
-    ddd($object->config('server'));
     return $object->config('server.url.' . $name . '.' . $object->config('framework.environment'));
 }
