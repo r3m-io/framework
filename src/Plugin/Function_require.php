8204a5b71fbbb48f8d90467a209e65d13005b49e
remco van der Velde <remco.pc@outlook.com>
2024-01-15 15:39:38 +0100

    found a bug in a template deeply required, where the exception does not show up.

--- src/Plugin/Function_require.php
+++ src/Plugin/Function_require.php
@@ -81,56 +81,50 @@ function function_require(Parse $parse, Data $data, $url='', $storage=[]){
             exec('chmod 640 ' . $cache_url);
         }
         $mtime = File::mtime($url);
-        try {
-            if(!empty($storage)){
-                $data_data = new Data();
-                $data_data->data($storage);
-                $data_data->data('r3m.io.parse.view.source.url', $url);
-                $data_data->data('ldelim', '{');
-                $data_data->data('rdelim', '}');
-                $parse->storage()->data('r3m.io.parse.view.source.mtime', $mtime);
-                $parser = new Parse($parse->object());
-                $compile =  $parser->compile($read, [], $data_data);
-                $data_script = $data_data->data('script');
-                $script = $data->data('script');
-                if(!empty($data_script) && empty($script)){
-                    $data->data('script', $data_script);
-                }
-                elseif(!empty($data_script && !empty($script))){
-                    foreach($script as $nr => $value){
-                        if(in_array($value, $data_script, true)){
-                            unset($script[$nr]);
-                        }
-                    }
-                    $data->data('script', array_merge($script, $data_script));
-                }
-                $data_link = $data_data->data('link');
-                $link = $data->data('link');
-                if(!empty($data_link) && empty($link)){
-                    $data->data('link', $data_link);
-                }
-                elseif(!empty($data_link && !empty($link))){
-                    foreach($link as $nr => $value){
-                        if(in_array($value, $data_link, true)){
-                            unset($link[$nr]);
-                        }
-                    }
-                    $data->data('link', array_merge($link, $data_link));
-                }
-                return $compile;
-            } else {
-                $source = $data->data('r3m.io.parse.view.source.url');
-                $data->data('r3m.io.parse.view.source.url', $url);
-                $parse->storage()->data('r3m.io.parse.view.source.mtime', $mtime);
-                $parser = new Parse($parse->object());
-                $result = $parser->compile($read, [], $data);
-                $data->data('r3m.io.parse.view.source.url', $source);
-                return $result;
+        if(!empty($storage)){
+            $data_data = new Data();
+            $data_data->data($storage);
+            $data_data->data('r3m.io.parse.view.source.url', $url);
+            $data_data->data('ldelim', '{');
+            $data_data->data('rdelim', '}');
+            $parse->storage()->data('r3m.io.parse.view.source.mtime', $mtime);
+            $parser = new Parse($parse->object());
+            $compile =  $parser->compile($read, [], $data_data);
+            $data_script = $data_data->data('script');
+            $script = $data->data('script');
+            if(!empty($data_script) && empty($script)){
+                $data->data('script', $data_script);
             }
-        }
-        catch (Exception $exception){
-            d($exception);
-            throw $exception;
+            elseif(!empty($data_script && !empty($script))){
+                foreach($script as $nr => $value){
+                    if(in_array($value, $data_script, true)){
+                        unset($script[$nr]);
+                    }
+                }
+                $data->data('script', array_merge($script, $data_script));
+            }
+            $data_link = $data_data->data('link');
+            $link = $data->data('link');
+            if(!empty($data_link) && empty($link)){
+                $data->data('link', $data_link);
+            }
+            elseif(!empty($data_link && !empty($link))){
+                foreach($link as $nr => $value){
+                    if(in_array($value, $data_link, true)){
+                        unset($link[$nr]);
+                    }
+                }
+                $data->data('link', array_merge($link, $data_link));
+            }
+            return $compile;
+        } else {
+            $source = $data->data('r3m.io.parse.view.source.url');
+            $data->data('r3m.io.parse.view.source.url', $url);
+            $parse->storage()->data('r3m.io.parse.view.source.mtime', $mtime);
+            $parser = new Parse($parse->object());
+            $result = $parser->compile($read, [], $data);
+            $data->data('r3m.io.parse.view.source.url', $source);
+            return $result;
         }
     } else {
         d('Require: file not found: ' . $url . ' in template: ' . $data->data('r3m.io.parse.view.source.url'));
