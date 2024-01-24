21e2a3a4117f74708d71bff9117de656c6121f8e
remco van der Velde <remco.pc@outlook.com>
2024-01-24 14:18:58 +0100

    update ln to be used with options -source -destination instead of parameters

--- src/Cli/Ln/Plugin/Function_ln.php
+++ src/Cli/Ln/Plugin/Function_ln.php
@@ -11,13 +11,23 @@ use R3m\Io\Exception\ObjectException;
 
 /**
  * @throws ObjectException
+ * @throws Exception
  */
-function function_ln(Parse $parse, Data $data){
+function function_ln(Parse $parse, Data $data): void
+{
     $object = $parse->object();
-
-    $source = App::parameter($object, 'ln', 1);
-    $target = App::parameter($object, 'ln', 2);
-
+    $options = App::options($object);
+    if(property_exists($options, 'source')){
+        $source = $options->source;
+    } else {
+        throw new Exception('Option "source" not found...');
+    }
+    if(property_exists($options, 'target')){
+        $target = $options->target;
+    }
+    else {
+        throw new Exception('Option "target" not found...');
+    }
     if(File::exist($target)){
         $exception = new Exception('File exists...');
         Event::trigger($object, 'cli.ln', [
@@ -25,7 +35,7 @@ function function_ln(Parse $parse, Data $data){
             'target' => $target,
             'exception' => $exception
         ]);
-        return;
+        throw $exception;
     }
     exec('ln -s ' . escapeshellarg($source) . ' ' . escapeshellarg($target));
     Event::trigger($object, 'cli.ln', [
