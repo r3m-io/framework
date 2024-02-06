665b40db4d7318db23168be3b0f875a8ab025053
remco van der Velde <remco.pc@outlook.com>
2024-02-06 18:12:34 +0100

    making a 404 error template

--- src/App.php
+++ src/App.php
@@ -230,7 +230,7 @@ class App extends Data {
                                     $object->config('ds') .
                                     'Exception' .
                                     $object->config('ds') .
-                                    '500.tpl';
+                                    '501.tpl';
                             }
                             $object->config(
                                 'controller.dir.root',
