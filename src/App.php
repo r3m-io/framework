63340e6479ad301f01454e66cd72c375d9773421
remco van der Velde <remco.pc@outlook.com>
2024-02-06 17:34:23 +0100

    making a 404 error template

--- src/App.php
+++ src/App.php
@@ -250,6 +250,7 @@ class App extends Data {
                                     $url,
                                     (object) [
                                         'exception' => (object) [
+                                            'class' => 'RouteException',
                                             'message' => '404 Not Found',
                                             'route' => $host . '/' . $object->request('request'),
                                             'file' => 'not-applicable',
