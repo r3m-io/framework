a1a25753a3a41cc81343b2c451749366b9afb686
remco van der Velde <remco.pc@outlook.com>
2024-02-08 16:55:48 +0100

    update exception files

--- src/Cli/Install/Controller/Install.php
+++ src/Cli/Install/Controller/Install.php
@@ -181,6 +181,7 @@ class Install extends Controller {
                 }
             }
         }
+        ddd($package);
         if(
             $package->has('route') &&
             is_array($package->get('route'))
