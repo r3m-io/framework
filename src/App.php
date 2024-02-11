6af052bade4092041a749708ad48de88b0848fba
remco van der Velde <remco.pc@outlook.com>
2024-02-11 16:43:47 +0100

    update exception files

--- src/App.php
+++ src/App.php
@@ -172,7 +172,6 @@ class App extends Data {
         $logger = false;
         if($object->config('framework.environment') === Config::MODE_DEVELOPMENT){
             $logger = $object->config('project.log.debug');
-            $object->logger($logger)->error('Debug started (' . $object->request('request') . ')...');
         }
         $logger_error =  $object->config('project.log.error');
         try {
