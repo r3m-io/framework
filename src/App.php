97569505c86223fa6066d7fb1d2a395621334691
remco van der Velde <remco.pc@outlook.com>
2024-02-11 08:50:44 +0100

    update exception files

--- src/App.php
+++ src/App.php
@@ -172,6 +172,7 @@ class App extends Data {
         $logger = false;
         if($object->config('framework.environment') === Config::MODE_DEVELOPMENT){
             $logger = $object->config('project.log.debug');
+            ddd($logger);
         }
         $logger_error =  $object->config('project.log.error');
         try {
