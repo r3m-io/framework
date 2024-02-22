a250fb8fb63c2891b4b99cdb392faff870742d19
remco van der Velde <remco.pc@outlook.com>
2024-02-22 19:02:39 +0100

    added require to the config

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -712,6 +712,7 @@ class Controller {
         $read = $parse->compile($read, $data, $parse->storage());
         Parse::readback($object, $parse, App::SCRIPT);
         Parse::readback($object, $parse, App::LINK);
+        d($object->config());
         return $read;
     }
 
