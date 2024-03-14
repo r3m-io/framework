d072ad9b12972a1a304f56b5972c082bdcc1b83c
remco van der Velde <remco.pc@outlook.com>
2024-03-14 16:34:55 +0100

    debug duration

--- src/App.php
+++ src/App.php
@@ -101,6 +101,8 @@ class App extends Data {
         App::is_cli();
         require_once __DIR__ . '/Debug.php';
         require_once __DIR__ . '/Error.php';
+        $duration = microtime(true) - $this->config('time.start');
+        ddd($duration);
         Config::configure($this);
         Logger::configure($this);
         Host::configure($this);
