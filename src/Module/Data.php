5f5758178365e18d6b0f13b8fa4acc37f8e0ae7e
remco van der Velde <remco.pc@outlook.com>
2024-03-19 20:04:17 +0100

    debug data

--- src/Module/Data.php
+++ src/Module/Data.php
@@ -35,6 +35,12 @@ class Data {
     public function __construct($data=null){
         if($data !== null){
             d($data);
+            $debug = debug_backtrace(1);
+            d($debug[0]['file'] . ' ' . $debug[0]['line'] . ' ' . $debug[0]['function']);
+            d($debug[1]['file'] . ' ' . $debug[1]['line'] . ' ' . $debug[1]['function']);
+            if(array_key_exists(2, $debug)){
+                d($debug[2]['file'] . ' ' . $debug[2]['line'] . ' ' . $debug[2]['function']);
+            }
         }
         $this->data($data);
     }
