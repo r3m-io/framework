407019f1a07478ee777ea80e2bfaed8debd68ec9
remco van der Velde <remco.pc@outlook.com>
2024-04-01 23:13:29 +0200

    p

--- src/Module/SharedMemory.php
+++ src/Module/SharedMemory.php
@@ -33,8 +33,8 @@ class SharedMemory {
                 0
             );
             $connect = @shmop_read($connect_shmop, 0, @shmop_size($connect_shmop));
-            ddd($connect);
-//            $connect = explode("\0", $connect, 2);
+            d($connect);
+            $connect = explode("\0", $connect, 2);
             $connect = Core::object($connect[0], Core::OBJECT_ARRAY);
         }
         catch (ErrorException $exception) {
