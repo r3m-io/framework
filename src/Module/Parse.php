441067438a0a2831a3d812857db53453fc2f0e35
remco van der Velde <remco.pc@outlook.com>
2024-04-12 13:14:21 +0200

    checking filter

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -486,6 +486,7 @@ class Parse {
                 ) {
                     $filter = $string->{'#output'}->filter;
                     d($filter);
+                    d($string);
                     $string->result = $string->{'#parallel'};
                     //parallel must be filtered because we delete #parallel from the object
 
