a83dff8e8a8d7088e463fd5c5f62080a6c42a472
remco van der Velde <remco.pc@outlook.com>
2024-04-12 13:55:05 +0200

    checking filter

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -420,7 +420,7 @@ class Parse {
                     } else {
 //                        $object->config('parse.disable.parallel', true);
 //                        $object->config('parse.disable.output', true);
-                        d($this->object()->config('parse.read.object.this.url'));
+                        d($file);
                         d($key);
                         $value = $this->compile($value, $storage->data(), $storage, $depth, $is_debug);
                         $string->{$key} = $value;
