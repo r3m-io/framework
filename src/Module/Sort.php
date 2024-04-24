344a62f79ef3acbe7559af480f493f4e1561eb59
remco van der Velde <remco.pc@outlook.com>
2024-04-24 10:56:49 +0200

    updated version

--- src/Module/Sort.php
+++ src/Module/Sort.php
@@ -69,6 +69,7 @@ class Sort extends Data {
                 foreach($list as $uuid => $node){
                     foreach($sort as $attribute => $record){
                         $value = $this->data($uuid . '.' . $attribute);
+                        d($value);
                         if(is_scalar($value)) {
                             if(is_array($node)){
                                 $result[$value][] = $node;
