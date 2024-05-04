7dd93354adbfa9e05d79d1509a6af7486d80d484
remco van der Velde <remco.pc@outlook.com>
2024-05-04 15:54:55 +0200

    add database::options
    -drop
    -truncate
    -rename

--- src/Module/Database.php
+++ src/Module/Database.php
@@ -224,6 +224,7 @@ class Database {
                 is_string($options->rename) ||
                 is_bool($options->rename)
             ){
+                ddd($options);
                 if($options->rename === true){
                     $options->rename = $table . '_old';
                     $counter = 1;
