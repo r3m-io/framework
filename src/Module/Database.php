11bcdc4c19091faf45b32521e1d3b40c05237116
remco van der Velde <remco.pc@outlook.com>
2024-05-14 12:16:37 +0200

    Database::tables, show all tables in the current table with $name (name of connection and environment which is autodetect

--- src/Module/Database.php
+++ src/Module/Database.php
@@ -380,6 +380,7 @@ class Database {
             $environment = '*';
             $connect = $object->config('doctrine.environment.' . $name . '.' . $environment);
             if(empty($connect)){
+                d($object->config('doctrine'));
                 d('no connect');
                 return false;
             }
