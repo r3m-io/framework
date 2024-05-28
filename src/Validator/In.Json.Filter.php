29df493d4976a7ed6281c0a4943e8e78d7504d8e
remco van der Velde <remco.pc@outlook.com>
2024-05-28 23:06:26 +0200

    filter can handle object as array

--- src/Validator/In.Json.Filter.php
+++ src/Validator/In.Json.Filter.php
@@ -19,6 +19,7 @@ use R3m\Io\Exception\FileWriteException;
 /**
  * @throws ObjectException
  * @throws FileWriteException
+ * @throws Exception
  */
 function validate_in_json_filter(App $object, $request=null, $field='', $argument='', $function=false): bool
 {
@@ -28,13 +29,6 @@ function validate_in_json_filter(App $object, $request=null, $field='', $argumen
     $ignore_case = $argument->ignore_case ?? false;
     $filter = $argument->filter ?? false;
     $key = $argument->key ?? false;
-    d($request);
-    d($field);
-    d($argument);
-    d($url);
-    d($list);
-    d($attribute);
-    d($filter);
     if($url === false) {
         return false;
     }
@@ -44,126 +38,39 @@ function validate_in_json_filter(App $object, $request=null, $field='', $argumen
         if($filter){
             if($key) {
                 $data_key = $data->data($key);
-                if ($data_key) {
+                if (!is_scalar($data_key)) {
                     $data_filter = Filter::list($data_key)->where($filter);
+                    if(
+                        is_array($data_filter) &&
+                        empty($data_filter)
+                    ){
+                        return false;
+                    }
                     if($data_filter === false){
                         $data_filter = Filter::record($data_key)->where($filter);
                     }
-                    ddd($data_filter);
+                    if($data_filter){
+                        return true;
+                    }
                 }
             } else {
                 $data_key = $data->data();
-                if($data_key){
+                if(!is_scalar($data_key)){
                     $data_filter = Filter::list($data_key)->where($filter);
+                    if(
+                        is_array($data_filter) &&
+                        empty($data_filter)
+                    ){
+                        return false;
+                    }
                     if($data_filter === false){
                         $data_filter = Filter::record($data_key)->where($filter);
                     }
-                    ddd($data_filter);
-                }
-            }
-            ddd('filter');
-        }
-    }
-
-    if(is_array($request)){
-        $data = $object->parse_read($url, sha1($url));
-        if($data){
-            $result = [];
-            if($list === false) {
-                $result[] = $data->get($attribute);
-            }
-            foreach($data->data($list) as $nr => $record) {
-                if (is_object($record)){
-                    $node = new Data($record);
-                    if(is_array($attribute)){
-                        foreach($attribute as $attr){
-                            if ($ignore_case) {
-                                $result[] = strtolower($node->get($attr));
-                            } else {
-                                $result[] = $node->get($attr);
-                            }
-                        }
-                    } else {
-                        if ($ignore_case) {
-                            $result[] = strtolower($node->get($attribute));
-                        } else {
-                            $result[] = $node->get($attribute);
-                        }
-                    }
-                } elseif(is_scalar($record)) {
-                    if($ignore_case){
-                        $result[] = strtolower($record);
-                    } else {
-                        $result[] = $record;
+                    if($data_filter){
+                        return true;
                     }
                 }
             }
-            foreach($request as $post){
-                if($ignore_case){
-                    $post = strtolower($post);
-                }
-                if(!in_array($post, $result, true)) {
-                    return false;
-                }
-            }
-            return true;
-        }
-    }
-    elseif(is_scalar($request)) {
-        $data = $object->parse_read($url, sha1($url));
-        if($data){
-            $result = [];
-            if($list === false) {
-                if(is_array($attribute)){
-                    $value = [];
-                    foreach ($attribute as $attr){
-                        $value[] = $data->get($attr);
-                    }
-                    $result[] = implode('', $value);
-                } elseif(is_scalar($attribute)){
-                    $result[] = $data->get($attribute);
-                }
-
-            } else {
-                foreach($data->data($list) as $nr => $record) {
-                    if (is_object($record)){
-                        $node = new Data($record);
-                        if(is_array($attribute)){
-                            $value = [];
-                             foreach($attribute as $attr){
-                                if ($ignore_case) {
-                                    $value[] = strtolower($node->get($attr));
-                                } else {
-                                    $value[] = $node->get($attr);
-                                }
-                             }
-                             $result[] = implode('', $value);
-                        } else {
-                            if ($ignore_case) {
-                                $result[] = strtolower($node->get($attribute));
-                            } else {
-                                $result[] = $node->get($attribute);
-                            }
-                        }
-                    }
-                    elseif(is_scalar($record)) {
-                        if ($ignore_case) {
-                            $result[] = strtolower($record);
-                        } else {
-                            $result[] = $record;
-                        }
-                    }
-                }
-            }
-            if($ignore_case){
-                $string = strtolower($request);
-            } else {
-                $string = $request;
-            }
-            if(!in_array($string, $result, true)) {
-                return false;
-            }
-            return true;
         }
     }
     return false;
