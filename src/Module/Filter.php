62d622b9a81d1fcdcf2a03a25e3c6031fcbaadf3
remco van der Velde <remco.pc@outlook.com>
2024-05-29 12:01:00 +0200

    filter can handle object as array

--- src/Module/Filter.php
+++ src/Module/Filter.php
@@ -284,6 +284,8 @@ class Filter extends Data {
                                     break;
                                 case Filter::OPERATOR_IN :
                                     $value = $data->get($attribute);
+                                    d($value);
+                                    d($record['value']);
                                     if (is_array($record['value'])) {
                                         if (is_scalar($value)) {
                                             if (
