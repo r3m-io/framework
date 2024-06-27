e5cb088b061a5383ef2f8e84fec7284855fd4d00
remco van der Velde <remco.pc@outlook.com>
2024-06-27 23:12:29 +0200

    added skip option to install (skipping cache-clear) for on entering the docker container

--- src/App.php
+++ src/App.php
@@ -1521,8 +1521,7 @@ class App extends Data {
                                         'ramdisk_url_record' => $url_ramdisk_record,
                                     ]);
                                 }
-                                if(File::exist($url_ramdisk_record) && $mtime !== $mtime_count){
-                                    d('mtime: ' . $mtime . ' record: ' . $mtime_count);
+                                if(File::exist($url_ramdisk_record) && $mtime !== $mtime_count){S
                                     File::write($url_ramdisk_record, Core::object($record, Core::OBJECT_JSON_LINE));
                                     File::permission($this, [
                                         'ramdisk_url_record' => $url_ramdisk_record,
