492ed1571f107129f6fdb796637c1b98afe27ff6
remco van der Velde <remco.pc@outlook.com>
2024-07-09 17:00:24 +0200

    line check for code instead of stringchar

--- src/Module/Parse/Value.php
+++ src/Module/Parse/Value.php
@@ -259,20 +259,19 @@ class Value {
                     }
                     $previous_pos = $position;
                 }
-                if(str_contains($lines[$nr], 'if(!in_array($command, User::COMMAND)){')){
-                    d($is_break);
-                    d($lines[$nr]);
-                    $lines[$nr] = str_replace($replace[$nr_contains][0], $replace[$nr_contains][1], $lines[$nr]);
-                    d($lines[$nr]);
-                    ddd($pos);
-                }
                 if($is_break === false){
                     d($lines[$nr]);
                     d($pos);
                     $lines[$nr] = str_replace($replace[$nr_contains][0], $replace[$nr_contains][1], $lines[$nr]);
                 }
             }
+            if(str_contains($lines[$nr], 'if(!in_array($command, User::COMMAND)){')){
+                d($is_break);
+                d($lines[$nr]);
+                ddd($pos);
+            }
         }
+
         return implode(PHP_EOL, $lines);
     }
 }
