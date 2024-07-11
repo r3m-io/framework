195b689c6b41b831072db7285c8db8a912c9ab7c
remco van der Velde <remco.pc@outlook.com>
2024-07-11 10:50:58 +0200

    line check for code instead of stringchar

--- src/Module/Parse/Build.php
+++ src/Module/Parse/Build.php
@@ -556,6 +556,7 @@ class Build {
                 }
                 if($exist === false){
                     trace();
+                    d($record);
                     ddd('end');
                     $text = $name . ' near ' . $record['value'] . ' on line: ' . $record['row'] . ' column: ' . $record['column'] . ' in: ' . $storage->data('source');
                     $exception = new PluginNotFoundException('Function not found: ' . $text, $dir_plugin);
