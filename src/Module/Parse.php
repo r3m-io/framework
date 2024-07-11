5b86a5b0167d0ae0a3b59737fbf0fa28787289eb
remco van der Velde <remco.pc@outlook.com>
2024-07-11 17:55:11 +0200

    line check for code instead of stringchar

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -331,7 +331,6 @@ class Parse {
         }
         if($type === 'array'){
             foreach($string as $key => $value){
-                d($value);
                 $value_type = gettype($value);
                 if(
                     $value === null ||
