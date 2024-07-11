a735348024a83a841024d9b7a40fdbcd2066f5d9
remco van der Velde <remco.pc@outlook.com>
2024-07-11 18:51:45 +0200

    literal::uniform

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -682,7 +682,6 @@ class Parse {
                             $string
                         );
                     }
-                    d($string);
                     if(empty($this->halt_literal())){
                         $string = Literal::restore($storage, $string);
                     }
@@ -864,14 +863,12 @@ class Parse {
                             $string
                         );
                     }
-                    d($string);
                     if (empty($this->halt_literal())) {
                         $string = Literal::restore($storage, $string);
                     }
                     if ($this->useThis() === true) {
                         $storage->data('delete', 'this');
                     }
-                    d($string);
                 } else {
                     $exception = new Exception('Class (' . $class . ') doesn\'t exist');
                     Event::trigger($object, 'parse.compile.exception', [
