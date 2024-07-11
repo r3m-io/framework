2471b1f292e1c094b7a80d3a93dedd370345aec9
remco van der Velde <remco.pc@outlook.com>
2024-07-11 17:44:36 +0200

    line check for code instead of stringchar

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -788,53 +788,53 @@ class Parse {
                         [
                             [
                                 'class',
-                                '{$ldelim}'
+                                '{'
                             ],
                             [
                                 'try',
-                                '{$ldelim}'
+                                '{'
                             ],
                             [
                                 '(',
-                                '{$ldelim}'
+                                '{'
                             ],
                             [
                                 'else',
-                                '{$ldelim}'
+                                '{'
                             ],
                             [
                                 Token::TYPE_WHITESPACE,
-                                '{$ldelim}'
+                                '{'
                             ],
                             [
                                 Token::TYPE_WHITESPACE,
-                                '{$rdelim}'
+                                '}'
                             ],
                         ],
                         [
                             [
-                                '{$ldelim}',
-                                '{$ldelim}' . PHP_EOL
+                                '{',
+                                '{' . PHP_EOL
                             ],
                             [
-                                '{$ldelim}',
-                                '{$ldelim}' . PHP_EOL
+                                '{',
+                                '{' . PHP_EOL
                             ],
                             [
-                                '{$ldelim}',
-                                '{$ldelim}' . PHP_EOL
+                                '{',
+                                '{' . PHP_EOL
                             ],
                             [
-                                '{$ldelim}',
-                                '{$ldelim}' . PHP_EOL
+                                '{',
+                                '{' . PHP_EOL
                             ],
                             [
-                                '{$ldelim}',
-                                '{$ldelim}' . PHP_EOL
+                                '{',
+                                '{' . PHP_EOL
                             ],
                             [
-                                '{$rdelim}',
-                                '{$rdelim}' . PHP_EOL
+                                '}',
+                                '}' . PHP_EOL
                             ]
                         ],
                         $string
