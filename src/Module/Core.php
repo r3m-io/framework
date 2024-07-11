05cdca0acab1bf69e3ae51a431758fe532681e62
remco van der Velde <remco.pc@outlook.com>
2024-07-11 10:55:29 +0200

    line check for code instead of stringchar

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -208,6 +208,7 @@ class Core
                 $option = Core::STREAM;
                 $object->config('core.execute.stream.is.default', true);
             }
+            ddd('fuck');
             //get option from $command
             switch($option){
                 case Core::FILE:
