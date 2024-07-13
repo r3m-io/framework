ca3ffdcd8bc5a3d7f08b80f688f588719b37f946
remco van der Velde <remco.pc@outlook.com>
2024-07-13 12:06:15 +0200

    bugfix parser, parser should leave text intact

--- src/Module/Parse/Token.php
+++ src/Module/Parse/Token.php
@@ -1160,7 +1160,6 @@ class Token {
         $depth = null;
         $attribute_nr = 0;
         $variable_nr = 0;
-        d($token);
         foreach($token as $nr => $record){
             if($record['type'] === Token::TYPE_METHOD){
                 $is_method = $nr;
@@ -1200,9 +1199,11 @@ class Token {
                     unset($token[$nr]);
                     continue;
                 }
+                /*
                 elseif($record['type'] === Token::TYPE_PARENTHESE_CLOSE){
                     continue;
                 }
+                */
                 elseif($record['type'] === Token::TYPE_PIPE){
                     $variable_nr++;
                     unset($token[$nr]);
