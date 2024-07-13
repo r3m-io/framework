1c0041c4d325706bb30918ed4359e75f857e18fc
remco van der Velde <remco.pc@outlook.com>
2024-07-13 14:14:32 +0200

    bugfix parser, parser should leave text intact

--- src/Module/Parse/Variable.php
+++ src/Module/Parse/Variable.php
@@ -433,6 +433,7 @@ class Variable {
         $set_counter = 0;
         $operator_max = 1024;
         $operator_counter = 0;
+        d($token);
         while(Set::has($token)){
             $set = Set::get($token);
             while(Operator::has($set)){
