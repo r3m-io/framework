c01017c9aa62e0953bc182245f8f8b3d132e3ec3
remco van der Velde <remco.pc@outlook.com>
2024-07-14 23:05:22 +0200

    bugfix parser, parser should leave text intact

--- src/Module/Parse/Variable.php
+++ src/Module/Parse/Variable.php
@@ -525,6 +525,7 @@ class Variable {
             }
             elseif($record['type'] === Token::TYPE_BRACKET_SQUARE_OPEN){
                 $in_array = true;
+                ddd($result);
                 if(substr($result, -3, 3) === ' . '){
                     $result = substr($result, 0, -3);
                 }
