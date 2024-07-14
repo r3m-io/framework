7e79c661de0dada0128b0c558ba0c2c3d6534721
remco van der Velde <remco.pc@outlook.com>
2024-07-14 10:53:15 +0200

    bugfix parser, parser should leave text intact

--- src/Module/Parse/Token.php
+++ src/Module/Parse/Token.php
@@ -1049,7 +1049,6 @@ class Token {
         array_pop($array); //remove square_close
         array_shift($array); //remove square_open
         $count = count($array);
-        d($count);
         foreach($array as $nr => $record){
             if($record['type'] === Token::TYPE_BRACKET_SQUARE_OPEN){
                 $selection = [];
