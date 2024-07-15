05e55db727d08767859c264c252eccf33c520745
remco van der Velde <remco.pc@outlook.com>
2024-07-15 16:47:45 +0200

    bugfix parser, parser should leave text intact

--- src/Module/Parse/Value.php
+++ src/Module/Parse/Value.php
@@ -308,6 +308,7 @@ class Value {
                 $result[] = '[';
                 foreach($record['value'] as $key => $value){
                     if(!array_key_exists('type', $value)){
+                        ddd($value);
                         $value = Value::array($build, $storage, $value);
                         d($value);
                     } else {
