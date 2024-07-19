1a5592ade718f5b7ad75490cecf480732b86687e
remco van der Velde <remco.pc@outlook.com>
2024-07-19 16:04:43 +0200

    bugfix parser, parser should leave text intact

--- src/Plugin/Function_script.php
+++ src/Plugin/Function_script.php
@@ -10,7 +10,6 @@ use R3m\Io\Module\Core;
 
 function function_script(Parse $parse, Data $data, $name='script', $script=null){
     $object = $parse->object();
-    ddd($script);
     if(is_array($script) || is_object($script)){
         return Core::object($script, Core::OBJECT_JSON);
     }
