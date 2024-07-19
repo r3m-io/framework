61f95a8e7081c1cdacf80e3c25222be64030c940
remco van der Velde <remco.pc@outlook.com>
2024-07-19 16:12:11 +0200

    bugfix parser, parser should leave text intact

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -576,10 +576,12 @@ class Parse {
             return $string;
         } else {
 //            d($string);
+            /*
             if(str_contains($string, 'Navigation.js')){
 //                trace();
                 d($string);
             }
+            */
             //this section takes at least 5 msec per document: file:put 2msec, opcache::put 2msec, rest 1msec
             $build = $this->build(new Build($this->object(), $this, $is_debug));
             $build->cache_dir($this->cache_dir());
