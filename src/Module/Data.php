982cca7aecffaef6ca55e014892547ae5198287e
remco van der Velde <remco.pc@outlook.com>
2024-07-28 23:09:01 +0200

    array_merge

--- src/Module/Data.php
+++ src/Module/Data.php
@@ -388,6 +388,7 @@ class Data {
                     $attribute = (string) $attribute;
                 }
                 if(is_string($attribute)){
+                    d($attribute);
                     return Core::object_get($attribute, $this->data(),$this->is_debug);
                 }
                 elseif(is_object($attribute) && get_class($attribute) === Data::class){
