138629a441191bb80d5e9e0eabfc651ccac44435
remco van der Velde <remco.pc@outlook.com>
2024-08-05 23:24:11 +0200

    is_hex

--- src/Module/Core.php
+++ src/Module/Core.php
@@ -2038,7 +2038,7 @@ class Core
     public static function is_hex($string=''): bool
     {
         if(strtoupper(substr($string, 0, 2)) === '0X'){
-            return ctype_xdigit(substr($string, 2);
+            return ctype_xdigit(substr($string, 2));
         }
         return ctype_xdigit($string);
     }
