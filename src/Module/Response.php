6f052da7837261d64205e8a3b93b009e0d5fccaf
remco van der Velde <remco.pc@outlook.com>
2024-08-22 23:58:34 +0200

    function trace

--- src/Module/Response.php
+++ src/Module/Response.php
@@ -168,7 +168,6 @@ class Response {
                 }
                 $json->script = $object->data(App::SCRIPT);
                 $json->link = $object->data(App::LINK);
-                ddd('end');
                 if($type === Response::TYPE_OBJECT_LINE){
                     return Core::object($json, Core::OBJECT_JSON_LINE);
                 } else {
