48d7c7e16dfd4e1b9af0ee6484c0f66a1fb6969e
remco van der Velde <remco.pc@outlook.com>
2024-08-25 21:47:39 +0200

    function trace

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -881,7 +881,7 @@ class Parse {
                     $string = $template->run();
                     $is_disabled = $this->object()->config('parse.compile.disable.function.Value::contains_replace');
 //                    $string = Parse::comment($string, 'is_disabled: ' . $is_disabled);
-//                    $is_disabled = true;
+                    $is_disabled = true;
                     if(!$is_disabled){
                         $string = Value::contains_replace(
                             [
