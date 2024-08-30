490e8b58d701e8a858716ab35136dd8868ef7d0e
remco van der Velde <remco.pc@outlook.com>
2024-08-30 21:59:08 +0200

    bugfix

--- src/Module/Controller.php
+++ src/Module/Controller.php
@@ -726,7 +726,6 @@ class Controller {
         if($ob){
             $read = $ob . $read;
         }
-        ob_end_clean();
         Parse::readback($object, $parse, App::SCRIPT);
         Parse::readback($object, $parse, App::LINK);
         return $read;
