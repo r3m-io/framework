f10748a80c2d66de93dcaeeb92930856def5a018
remco van der Velde <remco.pc@outlook.com>
2024-08-31 02:47:32 +0200

    bugfix

--- src/Module/Parse.php
+++ src/Module/Parse.php
@@ -667,6 +667,7 @@ class Parse {
                 try {
                     $template = new $class(new Parse($this->object()), $storage);
                     $string = trim($template->run());
+                    d('yes');
                     $is_disabled = $this->object()->config('parse.compile.disable.function.Value::contains_replace');
 //                    $is_disabled = true;
 //                    $string = Parse::comment($string, 'is_disabled: ' . $is_disabled);
